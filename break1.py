38af13a3f18deb487fba69af7397344c118f5011
Buse <busaydogdu77@gmail.com>
2024-11-24 09:35:38 +0300

    Delete break1.py

--- break1.py
+++
@@ -1,6 +0,0 @@
-for a in range(1,11):
-    if a == 3 : continue   #continue bu turu atla geç.
-    for b in range(1,11):
-        print(a,"x",b,"=",a*b)
-    print()   
-    if a == 5 : break  #break gördüğü yerde bitiriyor.
