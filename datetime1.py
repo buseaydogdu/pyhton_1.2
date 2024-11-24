0772a10424d66ac8b135690fa6616079e020095c
Buse <busaydogdu77@gmail.com>
2024-11-24 10:19:55 +0300

    Delete datetime1.py

--- datetime1.py
+++
@@ -1,5 +0,0 @@
-import datetime
-# print("Tarih saat =", datetime.datetime.now())
-
-print("Tarih saat =", datetime.datetime.now().strftime("%Y.%m"))
-print("Tarih saat =", datetime.datetime.now().strftime("%y.%m %D"))
