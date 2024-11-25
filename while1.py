4d40f4aa449dceb57b8c49b68aeebbdefa35476b
Buse <busaydogdu77@gmail.com>
2024-11-25 23:38:04 +0300

    Delete while1.py

--- while1.py
+++
@@ -1,9 +0,0 @@
-import random as rn
-renkler =  ["blue","red","tomato","al","purple"]
-for a in renkler: print(a)
-
-renk = ""
-while renk != "red":    #!= eşit değil demek
-    renk = rn.choice(renkler)
-    print(renk)
-
