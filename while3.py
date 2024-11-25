9482ba724e78f369616139cb1abdac0673ae1f98
Buse <busaydogdu77@gmail.com>
2024-11-25 23:38:38 +0300

    Delete while3.py

--- while3.py
+++
@@ -1,5 +0,0 @@
-r=5
-while r<100:   #while bir şart sağlanıyorsa altındaki kodlar çalıştırır.
-    print(r)
-    # r=r+1 = r += 1 de aynı şeyi yapar.
-    r += 5     # artırma işi yapılmazsa döngü sonsuz olur. sonsuz döngüyü kapatmak için ctrl+C ile durur.
