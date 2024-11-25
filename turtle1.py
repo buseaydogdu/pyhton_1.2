260168251e8c079f502e06ebe2b9ffd26821d589
Buse <busaydogdu77@gmail.com>
2024-11-25 23:37:45 +0300

    Delete turtle1.py

--- turtle1.py
+++
@@ -1,9 +0,0 @@
-import turtle
-# turtle dahili bir kütüphane olduğundan pip install turtle demeye gerek yok.
-for aa in range(3:)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.left(90)
-
-input()
